3987c22547ad1496b3aa8ad57609aba1a8a63c6d
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2021-10-23 20:32:28 +0100

    Fixed commit issue
    forgot to save some extra additions to user classes

--- SubscribedUser.py
+++ SubscribedUser.py
@@ -3,3 +3,6 @@ class SubscribedUser(User):
     def __init__(self, UserName, address, PassWord, Email, RentalStatus ):
         super().__init__(self,UserName,address,PassWord,Email)
         self.__RentalStatus = RentalStatus
+
+    def CancelSub():
+        return
--- User.py
+++ User.py
@@ -1,1 +1,12 @@
 class User:
+    
+    def __init__(self, UserName, address, PassWord, Email ):
+        self.__UserName = UserName
+        self.__address = address
+        self.__creditCardInfo = ""
+        self.__PassWord = PassWord
+        self.__Email = Email
+        self.IsPremium = False
+
+    def verifyLogIn():
+        return True
