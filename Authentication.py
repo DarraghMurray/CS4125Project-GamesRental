f26d00bfdbc98c0cbcb8708425003cfc6da0096d
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2021-10-25 21:48:12 +0100

    added basic class for sign-in/register

---
+++ Authentication.py
@@ -0,0 +1,9 @@
+class Authentication:
+    def __init__(self):
+        pass
+
+    def Register(self, Email, userName, passWord):
+        pass
+
+    def LogIn(self, Email, passWord):
+        pass
