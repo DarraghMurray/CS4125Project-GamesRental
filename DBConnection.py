ea74f1c77dcedec533078c38b58914f951aa5b3f
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2021-10-26 16:04:00 +0100

    added DBConnection to handle databse functions
    added config.py to store db credentials

---
+++ DBConnection.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+from config import Config
+
+class DBConnection:
+    def __init__(self):
+         self.db=mysql.connector.connect(
+            Config.DATABASE_CONFIG['user'],
+            Config.DATABASE_CONFIG['password'],
+            Config.DATABASE_CONFIG['host'],
+            Config.DATABASE_CONFIG['dbName']
+            )
+    
---
+++ config.py
@@ -0,0 +1,8 @@
+class Config(object):
+
+  DATABASE_CONFIG = {
+          'user': 'root',
+          'password': '',
+          'host': 'localhost',
+          'dbName': 'cs4125projdb',
+          }
