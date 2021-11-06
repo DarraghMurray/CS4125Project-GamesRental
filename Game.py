e3c1fd4e6a1b35fd2c171e482acbda36af566100
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2021-11-06 01:48:26 +0000

    added composite design pattern

--- Game.py
+++ Game.py
@@ -1,14 +1,15 @@
 
 from Price import Price
+from StoreItem import StoreItem
 
 
-class Game:
+class Game(StoreItem):
     def __init__(self, gameName, basePrice, rentalPrice, gameDescription):
         self.__gameName = gameName
         self.__basePrice = basePrice
         self.__rentalPrice = rentalPrice
         self.__gameDescription = gameDescription
 
-    def GetPrice(self):
-        Price.GetPrice()
+    def getPrice(self) -> float:
+        return self.__basePrice
 
