c87365027bb7e0faceb8aecf346f4068fbb78750
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2021-11-11 00:11:41 +0000

    made basic login/register GUI

---
+++ Controllers/NavigationController.py
@@ -0,0 +1,19 @@
+from Views import Navbar
+from Views import MainScreen
+
+class NavigationController:
+
+    def navigateToRegistration(self):
+        pass
+
+    def navigateToLogIn(self):
+        pass
+
+    def navigateToStore(self):
+        pass
+
+    def navigateToLibrary(self):
+        pass
+
+    def navigateToCart(self):
+        pass
---
+++ Views/RegisterUI.py
@@ -0,0 +1,40 @@
+
+import tkinter as tk
+from tkinter import font
+from tkinter.constants import SOLID, W
+
+class RegisterUI(tk.Frame):
+    def __init__(self,master):
+        tk.Frame.__init__(self,master)
+        self.font = ('Times', 14)
+        self.grid()
+        self.loadInterface()
+
+    def loadInterface(self):
+        self.loadLabels()
+        self.loadEntries()
+        self.loadButtons()
+
+    def loadLabels(self):
+        emailLabel = tk.Label(master=self,text="Enter Email", bg='#CCCCCC',font=self.font)
+        usernameLabel = tk.Label(master=self,text="Enter Username", bg='#CCCCCC',font=self.font)
+        passwordLabel = tk.Label(master=self,text="Enter Password", bg='#CCCCCC',font=self.font)
+
+        emailLabel.grid(row=0, column=0, sticky=W, pady=10)
+        usernameLabel.grid(row=1,column=0,pady=10)
+        passwordLabel.grid(row=2, column=0, pady=10)
+
+    def loadEntries(self):
+        email_tf = tk.Entry(master=self,font=self.font)
+        username_tf = tk.Entry(master=self,font=self.font)
+        pwd_tf = tk.Entry(master=self,font=self.font,show='*')
+        
+        email_tf.grid(row=0, column=1, pady=10, padx=20)
+        username_tf.grid(row=1, column=1, pady=10, padx=20)
+        pwd_tf.grid(row=2, column=1, pady=10, padx=20)
+
+    def loadButtons(self):
+        register_btn = tk.Button(master=self,width=15, text='Register', font=self.font, 
+                            relief=SOLID,cursor='hand2',command=None)
+        
+        register_btn.grid(row=3, column=1, pady=10, padx=20)
